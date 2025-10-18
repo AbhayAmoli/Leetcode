903015c75b2898612620a0533697620a1225ca9f
AbhayAmoli <109274307+AbhayAmoli@users.noreply.github.com>
2025-10-18 16:33:10 +0530

    Time: 133 ms (30.91%), Space: 97.7 MB (46.06%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,16 +1,17 @@
-
 class Solution {
-public:
-    int maxDistinctElements(vector<int>& nums, int k) {
-        ranges::sort(nums);
-        int ans = 0, pre = INT_MIN;
-        for (int x : nums) {
-            int cur = min(x + k, max(x - k, pre + 1));
-            if (cur > pre) {
-                ++ans;
-                pre = cur;
-            }
-        }
-        return ans;
-    }
+ public:
+  int maxDistinctElements(vector<int>& nums, int k) {
+    int ans = 0;
+    int occupied = INT_MIN;
+
+    ranges::sort(nums);
+
+    for (const int num : nums)
+      if (occupied < num + k) {
+        occupied = max(occupied + 1, num - k);
+        ++ans;
+      }
+
+    return ans;
+  }
 };
