1e3912f59e3a4dcb38e2bd846c8de94b8e182e2b
AbhayAmoli <109274307+AbhayAmoli@users.noreply.github.com>
2025-10-22 12:00:13 +0530

    Time: 1201 ms (43.13%), Space: 291.3 MB (42.18%) - LeetHub

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.cpp
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -1,28 +1,24 @@
 class Solution {
- public:
-  int maxFrequency(vector<int>& nums, int k, int numOperations) {
-    int ans = 1;
-    int adjustable = 0;
-    unordered_map<int, int> count;
-    map<int, int> line;
-    set<int> candidates;
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        unordered_map<int, int> frequency;
+        map<int, int> rangeBoundaries;
 
-    for (const int num : nums) {
-      ++count[num];
-      ++line[num - k];
-      --line[num + k + 1];
-      candidates.insert(num);
-      candidates.insert(num - k);
-      candidates.insert(num + k + 1);
+        for (int num : nums) {
+            frequency[num]++;
+            rangeBoundaries[num];
+            rangeBoundaries[num - k]++;
+            rangeBoundaries[num + k + 1]--;
+        }
+
+        int maxResult = 0;
+        int activeCount = 0; 
+   
+        for (const auto& [position, delta] : rangeBoundaries) {
+            activeCount += delta;
+            maxResult = max(maxResult, min(activeCount, frequency[position] + numOperations));
+        }
+
+        return maxResult;
     }
-
-    for (const int num : candidates) {
-      adjustable += line.contains(num) ? line[num] : 0;
-      const int countNum = count.contains(num) ? count[num] : 0;
-      const int adjusted = adjustable - countNum;
-      ans = max(ans, countNum + min(numOperations, adjusted));
-    }
-
-    return ans;
-  }
 };
